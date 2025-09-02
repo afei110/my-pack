79108140e4505148d50525b53bfdefa6734c7518
unknown <370791225@qq.com>
2025-09-02 14:33:59 +0800

    update

---
+++ src/MyClass.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Afei110/Mypack;
+
+class MyClass
+{
+	public function sayHello()
+	{
+		echo 'hello easy!';
+	}
+	
+	public static function sayYes()
+	{
+		echo 'yes,ooo';
+	}
+}
